37c4665a28d101a755e30e1d538e12ce826a668d
AlexxScript <ameneyroalex@gmail.com>
2024-10-19 22:14:17 -0600

    comenzando página de inicio

--- client-patli/src/app/page.tsx
+++ client-patli/src/app/page.tsx
@@ -1,8 +1,22 @@
+import Image from "next/image";
+import logo from "/public/icon.svg"
+
 export default function Home() {
   return (
-    <div className="my-[70px]">
-      <main className="">
-        <h1>patli</h1>    
+    <div className="">
+      <main className="max-w-5xl mx-auto grid grid-cols-6 gap-2">
+        <div className="col-start-2 col-span-4 flex justify-center items-center flex-col h-screen">
+          <Image 
+            src={logo}
+            alt="Patli"
+          />
+          <h1 className="font-bold">Prediciendo hoy para proteger tu mañana</h1>
+        </div>
+
+        <div className="col-start-1 col-span-3">Mision</div>
+        <div className="">Vision</div>
+        <div className="">Valores</div>
+        <div className="">otro</div>
       </main>
     </div>
   );
