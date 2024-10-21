c9516a93538f4c2cabc6b08372d71f725ee758d4
AlexxScript <ameneyroalex@gmail.com>
2024-10-20 21:09:33 -0600

    dockerizacion de aplicacion y base de datos para muestra

--- client-patli/src/app/page.tsx
+++ client-patli/src/app/page.tsx
@@ -1,19 +1,30 @@
 import Image from "next/image";
 import logo from "/public/icon.svg"
+import ilustration from "/public/ilustrationvector.svg"
 
 export default function Home() {
   return (
     <div className="">
-      <main className="max-w-5xl mx-auto grid grid-cols-6 gap-2">
+      <main className="max-w-5xl w-full mx-auto grid grid-cols-6 gap-2 max-[1024px]:max-w-3xl">
         <div className="col-start-2 col-span-4 flex justify-center items-center flex-col h-screen">
           <Image 
             src={logo}
             alt="Patli"
           />
-          <h1 className="font-bold">Prediciendo hoy para proteger tu mañana</h1>
+          <h1 className="text-center font-bold border-b-2 pb-2 title">Prediciendo hoy para proteger tu mañana</h1>
         </div>
 
-        <div className="col-start-1 col-span-3">Mision</div>
+        <div className="col-start-1 col-end-7 flex justify-around items-center">
+          <div className="w-[50%] flex justify-center">
+            <h2 className="text-3xl">¿Qué es patli?</h2>
+          </div>
+          <div className="w-[50%]">
+            <Image
+              src={ilustration}
+              alt="Dr ilustration"
+            />
+          </div> 
+        </div>
         <div className="">Vision</div>
         <div className="">Valores</div>
         <div className="">otro</div>
--- server-pahtli/pahtli_server/settings.py
+++ server-pahtli/pahtli_server/settings.py
@@ -11,12 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-# import environ
-# import os
-
-# env = environ.Env()
-# environ.Env.read_env(os.path.join(".env", '.env'))
-
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-9u1dhdmh#j)=u$o#56ki9&zcuf#u^9$w0dja^aed&!0_a-2@j0"
+SECRET_KEY = config('SECRET_KEY') 
+# SECRET_KEY = "django-insecure-9u1dhdmh#j)=u$o#56ki9&zcuf#u^9$w0dja^aed&!0_a-2@j0"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -102,12 +98,12 @@ WSGI_APPLICATION = "pahtli_server.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        'NAME': 'patli',
-        'USER': 'postgres',
-        'PASSWORD': 'mysecretpassword',
-        'HOST': 'localhost',
+        'NAME': config('POSTGRES_NAME'),
+        'USER': config('POSTGRES_USER'),
+        'PASSWORD': config('POSTGRES_PASSWORD'),
+        'HOST': config('HOST'),
         # 'HOST': 'db', #nombre del servicio de PostgreSQL definido en docker-compose.yml
-        'PORT': '5432',
+        'PORT': config('PORT'),
    
     }
 }
